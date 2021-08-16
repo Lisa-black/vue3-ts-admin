8fa738df5dbef3812557a716e0c93711b21ef904
向超 <ex_xiangchao@exiot.cmcc>
2021-08-16 10:58:23 +0800

    init

---
+++ src/views/user-manager/login/model/loginModel.ts
@@ -0,0 +1,11 @@
+// {
+//     "code": 0,
+//     "msg": "success",
+//     "data": {
+//         "accessToken": "admin-token"
+//     }
+// }
+
+export interface LoginModel {
+  accessToken: string
+}
